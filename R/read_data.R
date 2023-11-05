080b77f16945661fd161eb3363b45ab9480088ee
jun-hyuk-park <90289798+jun-hyuk-park@users.noreply.github.com>
2023-11-05 17:01:27 -0500

    Init data (#1)
    * gitignore, rproj
    
    * DESCRIPTION, MAN, R
    
    * DESCRIPTION, README, test update
    
    * read_data
    
    * Add example data
    
    * example data, read_data update
    
    ---------
    
    Co-authored-by: junhyukpark <jjunhyuk.park@mail.utoronto.ca>

---
+++ R/read_data.R
@@ -0,0 +1,23 @@
+#' Reads RNA copy counts data
+#'
+#' A function that reads RNA copy counts data
+#'
+#' @param file_name a name of RNA copy counts data. First column is Gene.
+#' Rest of columns are samples
+#' @param sep separator charactor in the file. ',' by as default.
+#'
+#' @return Returns a matrix
+#'
+#' @examples
+#' test1 <- c("Gene,Control1,Treatment1", "A,1,2", "B,3,4")
+#' tf <- tempfile()
+#' writeLines(test1, tf)
+#' RNA_data <- read_data(tf)
+#'
+#' @export
+read_data <- function(file_name, sep=",") {
+  RNA_l <- utils::read.csv(file_name, sep=sep, header=TRUE)
+  RNA_data <- as.matrix(RNA_l[,2:ncol(RNA_l)])
+  row.names(RNA_data) <- RNA_l$Gene
+  return(RNA_data)
+}
